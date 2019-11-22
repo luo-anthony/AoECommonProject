0c94fefca07859e5421422c1c8fc95598c01dc69
Dx724 <dx72dx@gmail.com>
2019-11-21 23:54:57 -0500

    Create ButtonAndLights.ino

---
+++ ButtonAndLights/ButtonAndLights.ino
@@ -0,0 +1,81 @@
+int PIN_BTN = 10;
+int SHIFT_DATA = 11;
+int SHIFT_CLK = 12;
+int SHIFT_LATCH = 13;
+int LIGHTS_DATA = 7;
+int LIGHTS_CLK = 8;
+int LIGHTS_LATCH = 9;
+int LIGHTS_DELTA = -1 * (SHIFT_DATA - LIGHTS_DATA);
+int LIGHTS_SHOWTIME = 0;
+
+int buttonReads[8];
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_BTN, INPUT);
+  pinMode(SHIFT_DATA, OUTPUT);
+  pinMode(SHIFT_CLK, OUTPUT);
+  pinMode(SHIFT_LATCH, OUTPUT);
+  pinMode(LIGHTS_DATA, OUTPUT);
+  pinMode(LIGHTS_CLK, OUTPUT);
+  pinMode(LIGHTS_LATCH, OUTPUT);
+  Serial.begin(9600);
+}
+
+void registerWrite(byte writeByte, int delta = 0) {
+  digitalWrite(SHIFT_LATCH + delta, LOW);
+  shiftOut(SHIFT_DATA + delta, SHIFT_CLK + delta, MSBFIRST, writeByte);
+  digitalWrite(SHIFT_LATCH + delta, HIGH);
+}
+
+void registerClear(int delta = 0) {
+  byte zero = 0;
+  registerWrite(zero, delta);
+}
+
+void registerSingle(int bitNum, int delta = 0) {
+  byte writeVal = 0;
+  bitWrite(writeVal, bitNum, 1);
+  registerWrite(writeVal, delta);
+}
+
+int[8] led_activeRow = {};
+void lights_drawRow() {
+  for (int i = 0; i < 8; i++) {
+    if (led_activeRow[i]) {
+      registerSingle(i, LIGHTS_DELTA);
+      delay(LIGHTS_SHOWTIME);
+    }
+  }
+}
+
+int[6][8] led_fullBoard = {};
+void lights_drawBoard() {
+  for (int rNum = 0; rNum < 6; rNum++) {
+    led_activeRow = led_fullBoard[rNum];
+    lights_drawRow();
+  }
+}
+
+void readButtons() { //Should debounce
+  for (int bNum = 0; bNum < 8; bNum++) {
+    registerSingle(bNum);
+    int readVal = digitalRead(PIN_BTN);
+    if (readVal != buttonReads[bNum]) {
+      delay(15);
+      readVal = digitalRead(PIN_BTN);
+    }
+    buttonReads[bNum] = readVal;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println(digitalRead(10));
+  readButtons();
+  for (int i = 0; i < 8; i++) {
+    Serial.print(String(buttonReads[i]) + "\t");
+  }
+  Serial.println("|");
+  delay(1);
+}
